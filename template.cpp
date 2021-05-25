271ccdea8e70d3ab2a050a758466fa0e033fb7e9
Hank Pan <hank@MacBook-Pro.local>
2021-05-25 22:41:13 +0800

    Add some essential files

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    // Solution solution;
+
+    return 0;
+}
