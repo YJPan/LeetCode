09e1e177a0cbe8eb9d504179a164eaf254aa04d6
Hank Pan <hank@MacBook-Pro.local>
2021-06-13 16:16:16 +0800

    Enhance build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-g++ $1 -o ans -std=c++11
+if [[ "$@" == *.cpp ]]; then
+    g++ $1 -o ans -std=c++11
+elif [[ "$@" == *.py ]]; then
+    python3 $1
+elif [[ "$@" == *.go ]]; then
+    go run $1
+else
+    TITLE="$@"
+    filename=`python3 -c "
+title = \"$TITLE\"
+num, name = title.split('. ')
+num = int(num)
+name = '_'.join(name.split()) + '.cpp'
+print(f'{num:04d}_{name}')
+    "`
+    echo "Generating $filename ..."
+    cp template.cpp $filename
+fi
