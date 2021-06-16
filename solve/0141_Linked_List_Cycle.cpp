30e7de29ead7414f4c2f8a1e085716d86b912aed
Hank Pan <hank@MacBook-Pro.local>
2021-06-16 20:12:49 +0800

    141. Linked List Cycle

---
+++ solve/0141_Linked_List_Cycle.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        while (head) {
+            if (head->val == INT_MAX)
+                return true;
+
+            head->val = INT_MAX;
+            head = head->next;
+        }
+
+        return false;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+    ListNode *head;
+
+    // Input: head = [3,2,0,-4], pos = 1
+    // Output: true
+    head = new ListNode(3);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(-4);
+    head->next->next->next->next = head->next;
+    cout << solution.hasCycle(head) << endl;
+
+    // Input: head = [1,2], pos = 0
+    // Output: true
+    head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = head;
+    cout << solution.hasCycle(head) << endl;
+
+    // Input: head = [1], pos = -1
+    // Output: false
+    head = new ListNode(1);
+    cout << solution.hasCycle(head) << endl;
+
+    return 0;
+}
