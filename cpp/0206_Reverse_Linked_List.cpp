f070f656ffe3f0eb317f8180bf1d7bcf20351866
Hank Pan <hank@MacBook-Pro.local>
2021-06-22 09:43:40 +0800

    206. Reverse Linked List

---
+++ cpp/0206_Reverse_Linked_List.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode ans;
+
+        while (head) {
+            ListNode* ptr = head;
+            head = head->next;
+
+            ptr->next = ans.next;
+            ans.next = ptr;
+        }
+
+        return ans.next;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+    ListNode* head;
+
+    // Input: head = [1,2,3,4,5]
+    // Output: [5,4,3,2,1]
+    head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    head = solution.reverseList(head);
+    while(head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+
+    // Input: head = [1,2]
+    // Output: [2,1]
+    head = new ListNode(1);
+    head->next = new ListNode(2);
+    head = solution.reverseList(head);
+    while(head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+
+    // Input: head = []
+    // Output: []
+    head = nullptr;
+    while(head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+
+    return 0;
+}
