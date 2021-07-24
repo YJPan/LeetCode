ae05ebc9781cbcb03067e7b07ac3ec25218e6187
Hank Pan <hank@MacBook-Pro.local>
2021-07-24 22:12:29 +0800

    395. Longest Substring with At Least K Repeating Characters

---
+++ cpp/0395_Longest_Substring_with_At_Least_K_Repeating_Characters.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        if (s.length() < k) return 0;
+
+        vector<int> record (26, 0);
+
+        for (auto c : s) {
+            record[c - 'a']++;
+        }
+
+        int l_end = 0;
+        while (l_end < s.length() && record[s[l_end] - 'a'] >= k)
+            l_end++;
+
+        int r_start = l_end + 1;
+        while (r_start < s.length() && record[s[r_start] - 'a'] < k)
+            r_start++;
+
+        if (l_end == s.length()) return s.length();
+
+        return max(longestSubstring(s.substr(0, l_end), k), longestSubstring(s.substr(r_start, s.length() - r_start), k));
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+    string s;
+
+    // Input: s = "aaabb", k = 3
+    // Output: 3
+    s = "aaabb";
+    cout << solution.longestSubstring(s, 3) << endl;
+
+    // Input: s = "ababbc", k = 2
+    // Output: 5
+    s = "ababbc";
+    cout << solution.longestSubstring(s, 2) << endl;
+
+    return 0;
+}
