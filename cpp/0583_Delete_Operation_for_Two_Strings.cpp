98a060e88fba68aadfdd02d949d4683af6a89765
Hank Pan <dhankb@gmail.com>
2021-11-13 11:47:58 +0800

    583. Delete Operation for Two Strings

---
+++ cpp/0583_Delete_Operation_for_Two_Strings.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.length() + 1, vector<int>(word2.length() + 1));
+        for (int i = 0; i <= word1.length(); i++) {
+            for (int j = 0; j <= word2.length(); j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = i + j;
+                else if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1;
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
+    }
+};
+
+int main(int argc, char *argv[]) {
+    // Solution solution;
+
+    return 0;
+}
