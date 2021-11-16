f51c7e392f03f4caa47e5e4ab709710919a12281
Hank Pan <dhankb@gmail.com>
2021-11-16 21:56:42 +0800

    25. Reverse Nodes in k-Group

---
+++ cpp/0025_Reverse_Nodes_in_k-Group.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int cnt = 0;
+        ListNode* new_head = head;
+        while (cnt < k) {
+            if (!new_head) break;
+            new_head = new_head->next;
+            cnt++;
+        }
+        if (cnt != k) return head;
+
+        ListNode ret;
+        ListNode* ptr = head;
+        while (--cnt >= 0) {
+            ListNode* next = ptr->next;
+            ptr->next = ret.next;
+            ret.next = ptr;
+            ptr = next;
+        }
+
+        head->next = reverseKGroup(new_head, k);
+
+        return ret.next;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    // Solution solution;
+
+    return 0;
+}
