94065bf8316389319b589c6354c3c3cdce163441
Hank Pan <dhankb@gmail.com>
2021-11-20 22:52:11 +0800

    Polish the code

--- cpp/0253_Meeting_Rooms_II.cpp
+++ cpp/0253_Meeting_Rooms_II.cpp
@@ -13,30 +13,25 @@ using namespace std;
 
 class Solution {
 public:
-    static bool comp(vector<int>& a, vector<int>& b) {
-        return (a[0] != b[0]) ? (a[0] < b[0]) : (a[1] < b[1]);
+    static bool comp(vector<int> &a, vector<int> &b) {
+        return a[0] < b[0];
     }
+
     int minMeetingRooms(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), comp);
-        vector<vector <int>> record;
 
-        for (auto i : intervals) {
-            bool addon = false;
-            for (auto &r : record) {
-                if (r[1] <= i[0]) {
-                    r[1] = i[1];
-                    addon = true;
-                    break;
-                }
-            }
+        priority_queue<int, vector<int>, greater<int>> end_time;
+        for (auto t : intervals) {
+            if (!end_time.empty() && end_time.top() <= t[0])
+                end_time.pop();
 
-            if (!addon)
-                record.push_back({i[0], i[1]});
+            end_time.push(t[1]);
         }
 
-        return record.size();
+        return end_time.size();
     }
 };
+
 
 int main(int argc, char *argv[]) {
     Solution solution;
