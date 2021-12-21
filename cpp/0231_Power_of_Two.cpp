5eb95311af65cf5dbb854ec9d88c9dd3bec0ac10
Hank Pan <dhankb@gmail.com>
2021-12-21 13:44:48 +0800

    231. Power of Two

---
+++ cpp/0231_Power_of_Two.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return (n & (n - 1)) == 0;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    // Solution solution;
+
+    return 0;
+}
