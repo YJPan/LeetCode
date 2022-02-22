cee029915dd4f82ecce063827fe941c1ba5fbacf
Hank Pan <dhankb@gmail.com>
2022-02-22 15:37:07 +0800

    200. Number of Islands

--- cpp/0200_Number_of_Islands.cpp
+++ cpp/0200_Number_of_Islands.cpp
@@ -13,13 +13,15 @@ using namespace std;
 class Solution {
 public:
     void dfs(vector<vector<char>>& grid, int x, int y) {
+        vector<vector<int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
         grid[x][y] = '0';
 
-        int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i], ny = y + dy[i];
-            if (nx < 0 || ny < 0 || nx >= grid.size() || ny >= grid[0].size() || grid[nx][ny] == '0')
-                continue;
+        for (auto d : dir) {
+            int nx = x + d[0];
+            int ny = y + d[1];
+
+            if (nx < 0 || ny < 0 || nx >= grid.size() || ny >= grid[0].size() || grid[nx][ny] != '1') continue;
 
             dfs(grid, nx, ny);
         }
@@ -27,12 +29,11 @@ public:
 
     int numIslands(vector<vector<char>>& grid) {
         int ret = 0;
-
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == '1') {
-                    ret++;
                     dfs(grid, i, j);
+                    ret++;
                 }
             }
         }
@@ -40,6 +41,47 @@ public:
         return ret;
     }
 };
+
+#if 0
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int ret = 0;
+        queue<vector<int>> q;
+        vector<vector<int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') {
+                    ret++;
+
+                    q.push({i, j});
+                    while (!q.empty()) {
+                        int size = q.size();
+                        for (int k = 0; k < size; k++) {
+                            int x = q.front()[0];
+                            int y = q.front()[1];
+                            q.pop();
+
+                            grid[x][y] = '0';
+                            for (auto d : dir) {
+                                int nx = x + d[0];
+                                int ny = y + d[1];
+
+                                if (nx < 0 || ny < 0 || nx >= grid.size() || ny >= grid[0].size() || grid[nx][ny] != '1') continue;
+
+                                q.push({nx, ny});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return ret;
+    }
+};
+#endif
 
 int main(int argc, char *argv[]) {
     Solution solution;
