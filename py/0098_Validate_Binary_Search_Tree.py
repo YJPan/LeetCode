2bc2e7b9695e8c877b6534a061ccc98558857dc7
Hank Pan <dhankb@gmail.com>
2023-06-10 19:57:55 +0800

    98. Validate Binary Search Tree

--- py/0098_Validate_Binary_Search_Tree.py
+++ py/0098_Validate_Binary_Search_Tree.py
@@ -1,8 +1,11 @@
 import sys
+import heapq
 from typing import *
+from collections import OrderedDict
 from collections import deque
 
 
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -11,35 +14,29 @@ class TreeNode:
 
 
 class Solution:
-    def recursion(self, root: TreeNode, small_node: TreeNode, big_node: TreeNode) -> bool:
-        if root is None:
+    def helper(self, root, l, r):
+        if not root:
             return True
 
-        if small_node and root.val <= small_node.val:
+        if l >= root.val or r <= root.val:
             return False
 
-        if big_node and root.val >= big_node.val:
-            return False
+        return self.helper(root.left, l, root.val) and self.helper(root.right, root.val, r)
 
-        return self.recursion(root.left, small_node, root) and self.recursion(root.right, root, big_node)
-
-    def isValidBST(self, root: TreeNode) -> bool:
-        return self.recursion(root, None, None)
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.helper(root, -sys.maxsize, sys.maxsize)
 
 
 if __name__ == "__main__":
     solution = Solution()
 
     root = TreeNode(2)
-    root.left = TreeNode(1)
-    root.right = TreeNode(3)
+    root.left, root.right = TreeNode(1), TreeNode(3)
     print(solution.isValidBST(root))
 
     root = TreeNode(5)
-    root.left = TreeNode(1)
-    root.right = TreeNode(4)
-    root.right.left = TreeNode(3)
-    root.right.right = TreeNode(6)
+    root.left, root.right = TreeNode(1), TreeNode(4)
+    root.right.left, root.right.right = TreeNode(3), TreeNode(6)
     print(solution.isValidBST(root))
 
     sys.exit(0)
